3735529c3f6db065b34ccd3100d322384a0273c9
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-05-17 10:58:29 -0400

    Fix minor bug in abs path resolution.

--- simplefsapp/simplefsapp.py
+++ simplefsapp/simplefsapp.py
@@ -25,7 +25,7 @@ class SimpleFSApp(ChrisApp):
     given by the --dir argument.
     """
     AUTHORS         = 'FNNDSC (dev@babyMRI.org)'
-    SELFPATH        = os.path.abspath(__file__)
+    SELFPATH        = os.path.dirname(os.path.abspath(__file__))
     SELFEXEC        = os.path.basename(__file__)
     EXECSHELL       = 'python3'
     TITLE           = 'Simple chris fs app'
